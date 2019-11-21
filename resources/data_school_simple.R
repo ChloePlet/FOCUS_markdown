77922aad92d83745d8617679046f611688cbb148
Plet <ple017@csiro.au>
2019-11-21 11:15:43 +0800

    Script simple for R-markdown project

---
+++ resources/data_school_simple.R
@@ -0,0 +1,63 @@
+library(tidyverse)
+library(RColorBrewer)
+library(cowplot)
+
+S_Cobar<-read_csv("data/NSWSouthCobar_Entity-WaterSample.csv")
+
+Geochem1<- read_csv("data/NSWGeochemistry1_Entity-WaterSample.csv")
+
+
+Geochem1_tidy<-Geochem1 %>%
+  select(7:9,12,14,17,20:27,42:45) %>%                               # xtract columns of interest
+  unite(1,2,3,4, col= "SampleID", sep="-")%>%                  # combine column to get sampleID
+  mutate("SampleName"=toupper(SiteName))%>%                    # homogenise samplename to capital
+  separate_rows(SampleName, sep=" ")  %>%                      # ????? need to split row into 2 when duplicate sampling
+  filter(str_detect(SampleName,"MX")|is.na(SampleName)) %>%    # need to delete row that do not correspond to samples
+  rename(m_asl=ElevationMetresAboveSeaLevel)%>%                 #rename column for clarity
+  mutate("StationDeposit"=toupper(StationDeposit)) %>% 
+  #mutate(WaterTable_depth_m = sub("[^0-9]*([0-9\\.]*).*","\\1",Minex_S_Cobar$WaterTable))
+  mutate(Conductivity= as.numeric(Conductivity))%>%
+  select(16,1,2,5:10,12:15)
+
+S_Cobar_tidy<-S_Cobar %>% 
+  select(7:9,12,14,17,20:27,42:45) %>%                               # xtract columns of interest
+  unite(1,2,3,4, col= "SampleID", sep="-")%>%                  # combine column to get sampleID
+  mutate("SampleName"=toupper(SiteName))%>%                    # homogenise samplename to capital
+  separate_rows(SampleName, sep=" ")  %>%                      # ????? need to split row into 2 when duplicate sampling
+  filter(str_detect(SampleName,"MX")|is.na(SampleName)) %>%    # need to delete row that do not correspond to samples
+  rename(m_asl=ElevationMetresAboveSeaLevel)%>%                 #rename column for clarity
+  mutate("StationDeposit"=toupper(StationDeposit)) %>% 
+  #mutate(WaterTable_depth_m = sub("[^0-9]*([0-9\\.]*).*","\\1",Minex_S_Cobar$WaterTable))
+  select(16,1,2,5:10,12:15)
+
+Obs_Field_NSW<-full_join(Geochem1_tidy, S_Cobar_tidy) %>% 
+    rename(pH = PreferredPH) %>% 
+    mutate(Eh = round(PreferredEh, digits = 0)) %>% 
+  select(-SampleID, - StationDeposit, -Accuracy, -m_asl, -PreferredEh)
+
+
+Temperature_plot<- ggplot(data = Obs_Field_NSW,
+       mapping=aes(x = Longitude, 
+                   y = Latitude,
+                   colour = Temperature,
+                  ))+
+  geom_point()+
+    scale_color_gradient2(
+      low = "lightblue1", mid = "royalblue1", high = "firebrick1",midpoint = 18, space = "Lab", guide = "colourbar", aesthetics = "colour")+
+
+
+
+Temperature_plot+
+  labs(title = "Water Temperature",
+     x = "longitude",
+     y = "latitude")
+
+pHEh_plot<- ggplot(data = Obs_Field_NSW,
+                mapping=aes(x = Longitude, 
+                            y = Latitude,
+                            size = PreferredPH,
+                            colour = PreferredEh ))+
+  geom_point(alpha = 0.4)+
+  scale_color_gradient2(low = "grey0", mid = "blue2", high = "deeppink1")
+  
+                
